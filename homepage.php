b2dc376ada8965b351b53380bbd35e69f16a46d8
Abdulaziz <azouz12344321@gmail.com>
2024-05-28 15:26:28 +0300

    compleat the clean  code for homepage

--- homepage.php
+++ homepage.php
@@ -27,11 +27,6 @@
 
 <br><br>
 
-<section id="last10AddedServices">
-  <br>
-<h1 class="headerForLast10AddedServices">Latest 10 Available Services</h1>
-<br>
-  <ul>
     <?php 
 
 include 'db_config.php';
@@ -40,81 +35,28 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
   }
 $phone =$_SESSION["phone"];  
+include 'functions/displayFunction.php';
 
+//displayLeastest
 $sql = "SELECT title,description,img_name,si.service_ID,phone,ui.name,numOfOrders
 FROM service_item as si join imagesofservice as ios on si.service_ID = ios.serviceID 
 join user_info as ui on ui.phoneNumber = si.phone order by si.service_ID desc";
-
+$numServicesShown=5;
+$type ='Last10AddedServices';
 $result = $con->query($sql);
+$title ="Latest 10 Available Services";
+displayServices($result,$numServicesShown,$type,$title);
 
-$myArray = [];
-$i=0;
-if ($result->num_rows > 0) {
-  while (($row = $result->fetch_assoc()) && $i<10) {
-      $sid = $row["service_ID"];
-      $tit = $row["title"];
-      $descr = $row["description"];
-      $na = $row["name"];
-      if(!in_array($sid,$myArray)){
-?>
-      <p>
-      <form method="post" action="servicePag.php">
-      <?php 
-      $imgName = $row["img_name"]; 
-      $myArray[] =$sid; 
-      echo "<a href ='servicePag.php?value=".$sid."' class='styleFora' ><li>
-      <img class='imgLast10AddedServicesContainer' src='img/$imgName' />
-      <h3 class='headerOfParagraphFontForLast10AddedServices'>$tit</h3><p class='paragraphFontLast10AddedServices'>$descr</p>
-      <p class='paragraphFontLast10AddedServices'>Service provider: $na</p></li></a></form>";
-      $i++;}}
-  }
-else {
-  echo "No results found.";
-}
-//$con->close();
-?>   </ul>
-</div>
-<br>
-</section>
 
-<section id="mostServicesOrder">
-  <br>
-<h1>Most Services Order</h1>
-<br>
-  <ul>
-    <?php 
-$sql = "SELECT title,description,img_name,si.service_ID,phone,ui.name,numOfOrders
+//display most order services
+$sql2 = "SELECT title,description,img_name,si.service_ID,phone,ui.name,numOfOrders
 FROM service_item as si join imagesofservice as ios on si.service_ID = ios.serviceID 
 join user_info as ui on ui.phoneNumber = si.phone order by numOfOrders desc";
+$numServicesShown=5;
+$type ='MostOrder';
+$result = $con->query($sql2);
+$title ="Most Services Order";
+displayServices($result,$numServicesShown,$type,$title);
 
-$result = $con->query($sql);
-$myArray2 = [];
-$i=0;
-if ($result->num_rows > 0) {
-  while (($row = $result->fetch_assoc()) && $i<5) {
-      $sid = $row["service_ID"];
-      $tit = $row["title"];
-      $descr = $row["description"];
-      $na = $row["name"];
-      if(!in_array($sid,$myArray2)){
 ?>
-      <p>
-      <form method="post" action="servicePag.php">
-      <?php 
-      $imgName = $row["img_name"]; 
-      $myArray2[] =$sid; 
-      echo "<a href ='servicePag.php?value=".$sid."' class='styleFora' >
-      <li><img class='imgForMostOrderContainer' src='img/$imgName' />
-      <h3 class='headerOfParagraphFontForMostOrder'>$tit</h3><p class='paragraphFontForMostOrder'>$descr</p>
-      <p class='paragraphFontForMostOrder'>Service provider: $na</p></li></a></form>";
-      $i++;}}
-  }
-else {
-  echo "No results found.";
-}
-$con->close();
-?>   </ul>
-</div>
-<br>
-</section>
 </body></html>
