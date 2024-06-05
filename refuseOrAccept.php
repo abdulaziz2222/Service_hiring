5fad1731d5922685c1dbffb12dc2610ee9e07fdf
Abdulaziz <azouz12344321@gmail.com>
2024-06-05 10:03:49 +0300

    test 75% of myAccount-db is not finish yet

--- refuseOrAccept.php
+++ refuseOrAccept.php
@@ -13,26 +13,14 @@
         $ord_id = $_POST["accept"];
         $query ="UPDATE `order` SET `condition` = 'accept' WHERE `order_service_id` = $ord_id";
         $result = mysqli_query($con,$query);
+        $_SESSION['statAcc']='accept';
     }else{
         $ord_id = $_POST["refuse"];
         $query ="UPDATE `order` SET `condition` = 'refuse' WHERE `order_service_id` = $ord_id";
         $result = mysqli_query($con,$query);
+        $_SESSION['statNotAcc']='refuse';
     }
         header("location:profile.php");
         mysqli_close($con);
-        /*
-         $result = mysqli_query($con,$query);
-        $query = "SELECT furniture_id FROM orderfur WHERE `order_id` = $ord_id";
-        $na = mysqli_query($con,$query);
-        if ($na) {
-            $rowN = mysqli_fetch_assoc($na);
-            $fid = $rowN['furniture_id'];
-            $query ="UPDATE `orderfur` SET `condition` = 'taken' WHERE `order_id` != $ord_id and furniture_id=$fid ";
-            $result = mysqli_query($con,$query);
-        } else {
-            // Handle query error
-            echo "Error executing the query: " . mysqli_error($con);
-        }    */
-        
         
 ?>
