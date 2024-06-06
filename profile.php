627dfbaa971c75e1e1004e583f340fb34b3a6060
Abdulaziz <azouz12344321@gmail.com>
2024-06-06 08:00:48 +0300

    finish profile page

--- profile.php
+++ profile.php
@@ -6,260 +6,22 @@
 <head>
     <meta charset="UTF-8">
     <title>My Account</title>
-    <link rel="stylesheet" href="homepage.css"> <!-- Ensure your CSS path is correct -->
-    <style>
-/* Overall body styles */
-body {
-    background-color: #9f9da7;
-    font-size: 1.6rem;
-    font-family: 'Open Sans', sans-serif;
-    color: #2b3e51;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+    <link rel="stylesheet" href="styles/homePage.css">
+    <link rel="stylesheet" href="styles/search.css">
+    <link rel="stylesheet" href="styles/barAndBackgroundImg.css">
+    <link rel="stylesheet" href="styles/footerStyle.css">    
+    <link rel="stylesheet" href="styles/myAccount2.css">    
+    <link rel="stylesheet" href="styles/myAccount.css">    
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
-/* Container for content to centralize and shadow */
-.container {
-    background-color: #fff;
-    font-weight: 300;
-    width: 80%;
-    margin: 0 auto;
-    text-align: center;
-    padding: 0px 0 20% 0;
-    border-radius: 4px;
-    box-shadow: 0px 30px 50px 0px rgba(0, 0, 0, 0.2);}
-
-/* Navigation bar styling */
-.topnav {
-    overflow: hidden;
-    background-color: #e9e9e9;
-    width: 100%;
-    border-radius: 4px 4px 0 0;
-}
-
-.topnav a {
-    float: left;
-    display: block;
-    color: black;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-}
-
-.topnav a.active {
-    background-color: #2196F3;
-    color: white;
-}
- .topnav a:hover {
-    background-color: #ddd;
-    color: black;
-}
-
-/* Styling for service lists */
-#servicesProvided, #servicesRequested {
-    margin: 20px auto;
-    width: 90%;
-}
-
-.topnav .search-container {
-  float: right;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    background: #f0f0f0;
-    margin: 10px 0;
-    padding: 10px;
-    border-radius: 8px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
-
-.service-details {
-    flex-grow: 1;
-}
-
-.service-actions {
-    flex-grow: 0;
-    flex-shrink: 0;
-}
-   
-.editButton {
-        padding: 8px 16px;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        background-color: #2196F3; /* Green for accept */
-        color: white;
-   /*     margin: 25px 120px; */
-}
-
-.accepTbutton {
-    padding: 8px 16px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    background-color: #4CAF50; /* Green for accept */
-    color: white;
-    margin-left: 5px;
-}
-
-.refusebutton{
-    padding: 8px 16px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    background-color: #4CAF50; /* Green for accept */
-    color: white;
-    margin-left: 5px;
-    background-color: #f44336; /* Red for refuse */
-}
-
-.topnav .search-container button {
-  float: right;
-  padding: 6px;
-  margin-top: 8px;
-  margin-right: 16px;
-  background: #ddd;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-
-button:hover {
-    opacity: 0.8;
-}
-
-.topnav input[type=text] {
-  padding: 6px;
-  margin-top: 8px;
-  font-size: 17px;
-  margin-right: 3px;
-  border: none;
-}
-@media screen and (max-width: 600px) {
-    .topnav a, .topnav input[type=text], .topnav .search-container button {
-        float: none;
-        display: block;
-        text-align: left;
-        width: 100%;
-        margin: 0;
-        padding: 14px;
-    }
-
-    .topnav input[type=text] {
-        border: 1px solid #ccc; 
-    }
-
-    li {
-        flex-direction: column;
-        align-items: start;
-    }
-
-    li img {
-        margin-bottom: 10px;
-    }
-
-    .service-actions {
-        width: 100%;
-        order: 3; /* Reorder to appear last */
-    }
-    
-
-}
-
-    /*styles for alert*/
-    .alert {
-        position: relative;
-        top: 10;
-        left: 0;
-        width: auto;
-        height: auto;
-        padding: 10px;
-        margin: 10px;
-        line-height: 1.8;
-        border-radius: 5px;
-        cursor: hand;
-        cursor: pointer;
-        font-family: sans-serif;
-        font-weight: 400;
-    }
-
-    .alertCheckbox {
-        display: none;
-    }
-
-    :checked+.alert {
-        display: none;
-    }
-
-    .alertText {
-        display: table;
-        margin: 0 auto;
-        text-align: center;
-        font-size: 16px;
-    }
-
-    .alertClose {
-        float: right;
-        padding-top: 5px;
-        font-size: 10px;
-    }
-
-    .clear {
-        clear: both;
-    }
-
-    .info {
-        background-color: #EEE;
-        border: 1px solid #DDD;
-        color: #999;
-    }
-
-    .success {
-        background-color: #EFE;
-        border: 1px solid #DED;
-        color: #9A9;
-    }
-
-    .notice {
-        background-color: #EFF;
-        border: 1px solid #DEE;
-        color: #9AA;
-    }
-
-    .warning {
-        background-color: #FDF7DF;
-        border: 1px solid #FEEC6F;
-        color: #C9971C;
-    }
-
-    .error {
-        background-color: #FEE;
-        border: 1px solid #EDD;
-        color: #A66;
-    }
-
-    </style>
 </head>
-<body>
+<body id="body" class="backgroundPhoto">
  <?php   
 include 'db_config.php';
 $con =mysqli_connect(DBHOST,DBUSER,DBPWD,DBNAME);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } ?>
-    <div class="container">
     <div class="topnav">
     <a class="active" href="homepage.php">Home</a>
     <a href="Profile.php">My account</a>
@@ -272,7 +34,33 @@ if ($con->connect_error) {
     </form>
     </div>
 </div>
-        <br>
+        <br><br><?php 
+            $userID=$_SESSION["id"];
+            $query = "SELECT phoneNumber,user_id,password,bio,userImg,name FROM user_info WHERE user_id='$userID'";
+            $np = mysqli_query($con,$query);
+            if ($np) {
+              $userRow = mysqli_fetch_assoc($np);
+              $userName=$userRow["name"];
+              $userBio=$userRow["bio"];
+              $userImg=$userRow["userImg"];
+            }else{die("connection to the database went wrong");}
+           /* profile */
+      echo  '<div id="profileBio">
+                <br>
+                <img src="'.$userImg.'" alt="Admin" class="rounded-circle" width="150">
+                  <h4>'.$userName.'</h4>
+                  <p class="text-secondary mb-1">'.$userBio.'</p>
+                  <br>
+                  <a href="editProfile.php" style=" margin: 25px 120px;"><button class="editButton" type="submit" 
+                  name="accept" value="">Edit my account</button></a>
+                  <a href="editServices.php" style=" margin: 25px 120px;"><button class="editButton" type="submit" 
+                  name="accept" value="">Edit my services</button></a>
+                  <br><br></div>';            
+            
+                /* Requests for my services */
+            ?><br>
+        <section id="searchPage">
+
         <?php     if (isset($_SESSION['status'])) {
         if ($_SESSION['status'] = "requested") {
             echo '
@@ -287,29 +75,59 @@ if ($con->connect_error) {
             unset($_SESSION['status']);
             unset($_SESSION['StartDate']); 
             unset($_SESSION['endDate']); 
+            /*  */
+          
+            if (isset($_SESSION['statAcc'])) {
+                  echo '<br>
+      <label>
+      <input type="checkbox" class="alertCheckbox" autocomplete="off" />
+      <div class="alert success">
+      <span class="alertClose">X</span>
+      <span class="alertText">You have successfully accepted the service request. Get ready to get started! <br /> 
+      <br class="clear"/></span>
+      </div>
+      </label>';
+      unset($_SESSION['statAcc']);
+    }
+      if(isset($_SESSION['statNotAcc'])){
+        echo '<br>
+        <label>
+        <input type="checkbox" class="alertCheckbox" autocomplete="off" />
+        <div class="alert error">
+        <span class="alertClose">X</span>
+        <span class="alertText">You have successfully declined the service request.
+        <br /> 
+        <br class="clear"/></span>
+        </div>
+        </label>';
+        unset($_SESSION['statNotAcc']);
+      }
+                  unset($_SESSION['stat']);
             ?>
-        <h2>Hi <?php echo $_SESSION["name"];?></h2>
-        <a href="editProfile.php" style=" margin: 25px 120px;"><button class='editButton' type='submit' name='accept' value='$oID'>Edit my account</button></a>
-        <a href="editServices.php" style=" margin: 25px 120px;"><button class='editButton' type='submit' name='accept' value='$oID'>Edit my services</button></a>
-        <div id="servicesRequested">
-            <h3>Requests for my services</h3>
+        <div id="servicesRequested"><br>
+            <h1>Requests for my services</h1><br><br>
             <ul>
                 <?php
                 $phon =$_SESSION['phone'];
-                $sql = 'SELECT ui.phoneNumber as ownerphone,si.title,description,img_name, o.phoneNumber as requestedphone
+                $sql = 'SELECT ui.phoneNumber as ownerphone,si.title as titleOfService,description,img_name,si.service_id,o.price,o.title as titleOfOrder
+                ,o.startDate,o.endDate	, o.phoneNumber as requestedphone
                 ,ui.name as ownerName,o.order_service_id, `condition` FROM `order` AS o JOIN service_item AS si ON o.service_ID = si.service_ID 
-                JOIN user_info AS ui ON si.phone = ui.phoneNumber Join imagesofservice as ios on si.service_ID = ios.serviceID  WHERE ui.phoneNumber = '."$phon".' ORDER BY order_service_id DESC';
+                JOIN user_info AS ui ON si.phone = ui.phoneNumber Join imagesofservice as ios on si.service_ID = ios.serviceID  
+                WHERE ui.phoneNumber = '."$phon".' ORDER BY order_service_id DESC';
                 $result = $con->query($sql);
                 $repeat ="";
                 if($result->num_rows >0 ){
                 while ($row = $result->fetch_assoc()) {
                     if($repeat != $row["order_service_id"]){
+                      $sid =$row["service_id"];
                     if($row["condition"] == 'waiting' && $_SESSION["phone"]= $row['ownerphone']){
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%; '>
+                    echo "<li> <a href='servicePag.php?value=" . $sid . "' class='styleFora'>
+                            <img src='img/".$row["img_name"]."' >
                             <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
+                                <h3>".$row["titleOfService"]."</h3>
+                                <p>The request: ".$row["titleOfOrder"]."</p>
+                                <p>Price: ".$row["price"]."</p>
+                                <p>The need of the service is from ".$row["startDate"]." to ".$row["startDate"]."</p>
                                 <p style='color: #FDDA0D;'>waiting</p>";
                                 $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["requestedphone"].'';
                                 $result2 = $con->query($sqlp);
@@ -317,42 +135,31 @@ if ($con->connect_error) {
                                 $oID = $row["order_service_id"];
                                 echo " <p>Requested by: ".$row2['name'].".</p>
                             </div>
-                            <div class='service-actions'>
+                            <div class='btnOfAccRef'>
                                 <form action='refuseOrAccept.php' method='post'>
                                     <input type='hidden' name='service_id' value='5'>
                                     <button class='accepTbutton' type='submit' name='accept' value='$oID'>Accept</button>
                                     <button class='refusebutton' type='submit' name='refuse' value='$oID'>Refuse</button>
                                 </form>
-                            </div>
+                            </div></a>
                         </li>";
                     }elseif ($row["condition"] == 'accept' && $_SESSION["phone"]= $row['ownerphone']){
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%;'>
+                    echo "<li> <a href='servicePag.php?value=" . $sid . "' class='styleFora'>
+                            <img src='img/".$row["img_name"]."'>
                             <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
-                                <p style='color: #13a809;'>Accepted</p>";
+                            <h3>".$row["titleOfService"]."</h3>
+                            <p>The request: ".$row["titleOfOrder"]."</p>
+                            <p>Price: ".$row["price"]."</p>
+                            <p>The need of the service is from ".$row["startDate"]." to ".$row["startDate"]."</p>
+                            <p style='color: #13a809;'>Accepted</p>";
                                 $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["requestedphone"].'';
                                 $result2 = $con->query($sqlp);
                                 $row2 = $result2->fetch_assoc();
                                 echo " <p>Requested by: ".$row2['name'].".</p>
                                 <p>".$row2['name']." phone number: ".$row['requestedphone']."</p>
-                            </div> 
+                            </div> </a>
                             </li>";
                 
-            }else{
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%;'>
-                            <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
-                                <p style='color: red;'>Refused</p>";
-                                $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["requestedphone"].'';
-                                $result2 = $con->query($sqlp);
-                                $row2 = $result2->fetch_assoc();
-                                echo " <p>Requested by: ".$row2['name'].".</p>
-                            </div> 
-                            </li>";
             }
             $repeat=$row["order_service_id"];
         }}}else {
@@ -360,71 +167,20 @@ if ($con->connect_error) {
         }
         
                 ?>
-            </ul>
-            <h3>My requests for services</h3>
-            <ul>
-                <?php
-                $phon =$_SESSION['phone'];
-                $sql = 'SELECT ui.phoneNumber as ownerphone,si.title,description,img_name, o.phoneNumber as requestedphone
-                ,ui.name as ownerName,o.order_service_id, `condition` FROM `order` AS o JOIN service_item AS si ON o.service_ID = si.service_ID 
-                JOIN user_info AS ui ON si.phone = ui.phoneNumber Join imagesofservice as ios on si.service_ID = ios.serviceID  WHERE o.phoneNumber =  '."$phon".'  ORDER BY order_service_id DESC';
-                $result = $con->query($sql);
-                $repeat ="";
-                // Assume $resultRequested is fetched before this point
-                if($result ->num_rows >0){
-                while ($row = $result->fetch_assoc()) {
-                    if($repeat != $row["order_service_id"]){
-                    if($row["condition"] == 'waiting' && $_SESSION["phone"]= $row['requestedphone']){
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%; '>
-                            <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
-                                <p style='color: #FDDA0D;'>waiting</p>";
-                                $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["ownerphone"].'';
-                                $result2 = $con->query($sqlp);
-                                $row2 = $result2->fetch_assoc();
-                                $oID = $row["order_service_id"];
-                                echo " <p>service provider: ".$row2['name'].".</p>
-                            </div>
-                            
-                        </li>";
-                    }elseif ($row["condition"] == 'accept' && $_SESSION["phone"]= $row['requestedphone']){
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%;'>
-                            <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
-                                <p style='color: #13a809;'>Accepted</p>";
-                                $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["ownerphone"].'';
-                                $result2 = $con->query($sqlp);
-                                $row2 = $result2->fetch_assoc();
-                                echo " <p>Service provider: ".$row2['name'].".</p>
-                                <p>".$row2['name']." phone number: ".$row['ownerphone']."</p>
-                            </div> 
-                            </li>";
-                
-            }else{
-                    echo "<li> 
-                            <img src='img/".$row["img_name"]."' style='width: 15%;'>
-                            <div class='service-details'>
-                                <h3>".$row["title"]."</h3>
-                                <p>".$row["description"]."</p>
-                                <p style='color: red;'>Refused</p>";
-                                $sqlp ='SELECT name FROM user_info where phoneNumber='.$row["ownerphone"].'';
-                                $result2 = $con->query($sqlp);
-                                $row2 = $result2->fetch_assoc();
-                                echo " <p>Service provider: ".$row2['name'].".</p>
-                            </div> 
-                            </li>";
-            }
-            $repeat=$row["order_service_id"];
-        }}} else{
-            echo '<p>No Result Found.</p>';
-        }
-                ?>
-            </ul>
+            </ul><br>
         </div>
-    </div>
+    </section>
+    <?php     
+    include 'functions/footer.php';
+    //Footer
+    footer();  
+    ?>
 </body>
 </html>
+
+
+
+
+
+
+
